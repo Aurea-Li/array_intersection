e963babed654051d3ca76d452a2441c13f122c8f
Aurea <fu.sakura@gmail.com>
2018-09-17 16:41:12 -0700

    implement solution for intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,23 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  return [] if array1.nil? || array2.nil? || array1.empty? || array2.empty?
+
+  length1 = array1.length
+  length2 = array2.length
+
+
+
+    hh = {}
+    array1.each do |num|
+      hh[num] = 0
+    end
+
+    intersect = []
+    array2.each do |num|
+      intersect << num if hh[num]
+    end
+
+
+  return intersect
+
 end
